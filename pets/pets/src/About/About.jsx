4a20efd9216dbae8375bb3410106c0904fd687b4
Astha Singh <singhastha182004@gmail.com>
2024-03-21 22:46:33 +0530

    contact us added

--- pets/pets/src/About/About.jsx
+++ pets/pets/src/About/About.jsx
@@ -4,21 +4,58 @@ import './About.css';
 
 const About = () => {
   return (
-    <div classname='greetings'>
-      <img src='' alt=''/>
-      <h1><i><b>Greetings from Petshub</b></i></h1>
-      <hr/>
-
+    <div className="about"> 
+    <div className="about-container">
+    <div className='about-box'>
+    <h1 className='greetings'><i><b>Greetings from Petshub</b></i></h1>
+    <img src='' alt=''/>
       <p>
       Every creature deserves a loving home, and that is our belief at Paws for Love. Our goal is to rescue, rehab, and rehome animals in need while eventually fostering enduring relationships between dogs and their adopted families. It is a straightforward but significant mission statement.
       </p>
-    {/* <img src={about} alt=''/>*/}
+      </div>
 
+      <div className="about-box">
+        <h1 className='h2'>Our Goals</h1>
+      
+        <p>Our goal at Pethub is to rescue, rehabilitate, and rehome pets to kind and considerate families who will treasure them for the rest of their lives. It's a straightforward yet powerful mission statement. We are dedicated to encouraging ethical pet ownership and lowering the quantity of stray animals in our neighborhood.</p>
+
+      </div>
+
+      <div className="about-box">
+        <h1 className="h3">Values</h1>
+        
+        <p>At Pethub, our principles guide all we do. Our activities are guided by compassion, honesty, and openness as we work to give each animal in our care the best possible care. Regardless of the history or current situation, we think every creature deserves to be treated with respect and decency.</p>
+      </div>
+
+      <div className="about-box">
+        <h1 className='h4'>Adoption Process</h1>
+       
+        <p>We have a procedure in place to make sure that adopters and dogs are matched as closely as possible. We are available to assist you at every stage, from initial questions to post-adoption care. Examine our available animals, fill out an adoption form, and get ready to bring a new pet into your house!</p>
+      </div>
+
+      <div className="about-box">
+        <h1 className='h5'>Impact</h1>
+        
+        <p>
+         Pethub has transformed the lives of adopters and animals alike by our unwavering work and the community's support. Every triumph, from touching adoption tales to accomplished rescue operations, increases our fervor and dedication to our cause
+        </p>
+      </div>
+
+      <div className="about-box">
+        <h1 className='h6'>Get Involved</h1>
+       
+        <p>
+        Are you prepared to change things? Pawsome Pals offers an endless array of opportunities for involvement, ranging from fostering and volunteering to making donations and promoting pet adoption. Come along with us as we work to save lives and provide needy creatures better futures.
+        </p>
+      </div>
+    </div>
+   
 
       
       
       
     </div>
+    
   )
 }
 
--- pets/pets/src/ContactUs/ContactUs.jsx
+++ pets/pets/src/ContactUs/ContactUs.jsx
@@ -3,8 +3,50 @@ import React from 'react'
 
 const ContactUs = () => {
   return (
-    <div>
-      
+    <div className='box'>
+      <div className='top'>
+        <div className='para'>
+          <p>We're here to help you find your perfect furry friend. Please feel free to reach out to us with any questions, inquiries, or feedback.</p>
+        </div>
+        <hr/>
+        <div className='Mailing Address'>
+          <h3>Mailing Address:</h3>
+          <p>
+           123 Pet Adoption Street<br/>
+           Cityville, State, ZIP Code<br/>
+           Country</p>
+        </div>
+        <div className="Phone">
+          <h3>Phone:</h3>
+          <p>
+          (555) 123-4567</p>
+        </div>
+        <div className='Email'>
+          <h3>Email:</h3>
+          <a href="#" >info@petadoptionmumbai.in</a>
+        </div>
+        <h3 className='top-line'>Reach Out To Us At:</h3>
+        
+        <div className="handles">
+          <ul>
+            <li>Instagram</li>
+            <li>Facebook</li>
+            <li>Twitter</li>
+            <li>Email</li>
+          </ul>
+        </div>
+        <div className="operating-hrs">
+          <h3>Operating Hours</h3>
+          <p>Monday-Friday: 9:00am - 5:00pm</p>
+          <p>Saturday-Sunday:Closed</p>
+        </div>
+        <div className="response-time">
+          <h3>Response Time:</h3>
+          <p>We strive to respond to all inquiries within 24-48 hours during our regular operating hours</p>
+
+          </div>
+      </div>
+
     </div>
   )
 }
